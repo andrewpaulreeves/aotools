7da182342397ab99921c72fde545fa1fd67ba5fa
Andrew Reeves <andrewpaulreeves@gmail.com>
2026-06-05 16:19:53 +0100

    update setup structure: - move to pyproject - move to setuptools-csm for version management

--- aotools/__init__.py
+++ aotools/__init__.py
@@ -7,5 +7,4 @@ from .interpolation import *
 from .turbulence import *
 from .image_processing import *
 
-from . import _version
-__version__ = _version.get_versions()['version']
+from ._version import __version__
